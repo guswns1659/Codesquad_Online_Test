c7adaa34381f21ce9415582410cb52aeeeeb6edb
Son <guswns1659@gmail.com>
2019-11-29 17:15:36 +0900

    [step1 Eighth] Complete step1

--- step1.js
+++ step1.js
@@ -1,16 +1,15 @@
 // Baseball Game 
 // By Hyunjun
-// 2019.11.29
 
 
 // Score Object
 // Intialize condition and change condition.
-score = {scoreList : ['스트라이크', '볼', '안타', '아웃'],
+score = {conditionList : ['스트라이크', '볼', '안타', '아웃'],
          strikeCount : 0,
          ballCount : 0,
          hitCount : 0,
          outCount : 0,
-         playCount : 0,
+         playCount : 1,
 };
 
 // Accumulate condition
@@ -30,11 +29,11 @@ score.print = function () {
         console.log(`최종 안타 수 : ${this.hitCount}개 입니다.\nGAME OVER`);
     } else if(this.isOutOrHit()) {
         console.log(`${this.condition}! 다음 타자가 타석에 입장했습니다.`);
-        this.scoreInit(); 
+        this.conditionInit(); 
         console.log(`${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
     } else if(this.is3StrikeOr4Ball()) {
         console.log(`${this.condition}\n아웃! 다음 타자가 타석에 입장했습니다.`)
-        this.scoreInit(); 
+        this.conditionInit(); 
         console.log(`${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
     } else {
         console.log(`${this.condition}!\n${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
@@ -59,29 +58,29 @@ score.is3StrikeOr4Ball = function () {
 // Checking progress of game and restarting game
 score.progress = function () {
     if (this.isOver()){
-        this.scoreInit();
+        this.conditionInit();
         this.print();
     } else if (this.isOutOrHit()) {
         this.print();
-        // this.init();
+        this.init();
     } else if(this.is3StrikeOr4Ball()) {
         this.print();
-        // this.init();
+        this.init();
     } else if(this.condition === '스트라이크' || this.condition ==='볼'){
         this.print();
-        // this.init();
+        this.init();
     }
 }
 
-// When player changed, initialize strikeCount and ballCount 
-score.scoreInit = function () {
+// When player is changed, initialize strikeCount and ballCount 
+score.conditionInit = function () {
     this.strikeCount = 0;
     this.ballCount = 0;
 }
 
 
-// Update count of score
-score.updateScore =function () {
+// Update count of condition
+score.updateCondition =function () {
     if(this.condition === '스트라이크') {
         this.strikeCount++;
     } else if(this.condition === '볼') {
@@ -94,22 +93,22 @@ score.updateScore =function () {
 }
 
 
-// Pick random score in scoreList 
+// Pick random score in conditionList 
 score.pickRandomCondition = function (){
     const randomNum = Math.floor(Math.random() * 4); // 0, 1, 2, 3
-    this.condition = this.scoreList[randomNum] // 스크라이크
+    this.condition = this.conditionList[randomNum] 
 };
 
-// starting game 
+// Starting game 
 score.init = function () {
-    if(this.playCount === 0) {
+    if(this.playCount === 1) {
         this.playCount++;
         console.log(`신나는 야구게임!`);
         console.log(`첫 번째 타자가 타석에 입장했습니다.\n`);
     }
     this.pickRandomCondition();
-    this.updateScore(); // strikeCount++;
-    this.checkAccumulation(); // 스트라이크 3개면 아웃 1개
+    this.updateCondition(); 
+    this.checkAccumulation(); 
     this.progress();
 
 }
