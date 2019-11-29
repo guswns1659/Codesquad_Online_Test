43b95b857ea4166600a262d8849ca798289c5b16
Son <guswns1659@gmail.com>
2019-11-29 19:52:05 +0900

    [step2 Second] Team1의 팀명, 타자들의 이름 및 타율 요청
    -타자의 이름과 타율을 동시가 아닌  순차적으로 입력할 수 있게 구현
    -askCount를 1로 초기화해 Team1의 정보를 다 입력하면 ++1 하여 Team2 물어볼 수 있게 구현

--- step2.js
+++ step2.js
@@ -1,8 +1,10 @@
 // input객체
 // team1과 team2 선수들의 이름과 타율을 저장 
 input={batterName1 : [],
-       battingAvg1 : []
-
+       battingAvg1 : [],
+       batterName2 : [],
+       battingAvg2 : [],
+       askCount : 1
 };
 
 // 데이터 입력 인지 출력인지 물어보는 메소드 
@@ -14,6 +16,7 @@ input.askChoice = function() {
 input.checkChoice = function() {
     if (this.inputOrOutput === 1 ){
         this.teamName();
+        this.askBatterInfo();
     } else if (this.inputOrOutput === 2) {
         
     } else {
@@ -24,13 +27,45 @@ input.checkChoice = function() {
 
 // 팀이름 입력을 요청하는 메소드
 input.teamName = function () {
-    this.teamName1 = prompt('1팀의 이름을 입력하세요');
+    if(this.askCount===1){
+        this.teamName1 = prompt('1팀의 이름을 입력하세요');
+    } else {
+        this.teamName2 = prompt('2팀의 이름을 입력하세요');
+    }
 }
 
-// 타자 정보 입력을 요청하는 메소드
+//team1에게 정보 물어보는 메소드
+input.askToTeam1 = function () {
+    for(let i = 0; i<2; i++){
+        let batterName = prompt(`${i+1}번 타자 이름 입력`);
+        let battingAvg = prompt(`${i+1}번 타자 타율 입력`);
+        this.batterName1.push(batterName);
+        this.battingAvg1.push(battingAvg);
+        
+    }
+
+}
+
+//team2에게 정보 물어보는 메소드
+input.askToTeam2 = function () {
+    for(let i = 0; i<2; i++){
+        let batterName = prompt(`${i+1}번 타자 이름 입력`);
+        let battingAvg = prompt(`${i+1}번 타자 타율 입력`);
+        this.batterName2.push(batterName);
+        this.battingAvg2.push(battingAvg);   
+    }
+}
+
+// 타자 이름 입력을 요청하는 메소드
 input.askBatterInfo = function () {
-
+    if(this.askCount==1) {
+        this.askToTeam1();
+    } else {
+        this.askToTeam2();
+    }
+    this.askCount++;
 }
+
 
 // 프로그램 시작 메소드
 function init() {
