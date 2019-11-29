bfa05eb05536c8dd2d1b3afcfb085f8c7b68299b
Son <guswns1659@gmail.com>
2019-11-29 15:55:32 +0900

    [step1 Fifth] Fix bugs
    Bugs : Execute infinite loop.
    So, i fix score.print method

--- step1.js
+++ step1.js
@@ -8,43 +8,61 @@ score = {scoreList : ['스트라이크', '볼', '안타', '아웃'],
          playCount : 0
 };
 
-// Print score
-score.print = function (score) {
-    console.log(score);
-    if(this.isPlayerFinished()) {
-        console.log('다음 타자가 타석에 입장했습니다.');
-    } 
-    if(this.isOver()) {
-        console.log(`안타 개수 : ${this.hitCount} 입니다.`);
-    }
-    console.log(`${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
-};
 
 // Checking how many accumulate count of score
 // ex) 3 strikeCount === 1 outCount
-score.checkAccumulate = function () {
+score.checkAccumulation = function () {
     if(this.strikeCount === 3){
         this.outCount++;
     } else if(this.ballCount === 4) {
         this.hitCount++;
     }
 }
+// Print score
+score.print = function (score) {
+    // console.log(score);
+    if(this.isOver()) {
+        console.log(`${score}!\n${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
+        console.log(`안타 개수 : ${this.hitCount} 입니다.`);
+    } else if(this.isPlayerFinished(score)) {
+        console.log(`${score} 다음 타자가 타석에 입장했습니다.`);
+        console.log(`${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
+    } else {
+        console.log(`${score}\n${this.strikeCount}S ${this.ballCount}B ${this.outCount}O`);
+    }
+};
 
 // Checking game is over or not
 score.isOver = function () {
     return this.outCount === 3;
 }
 
-// When player changed, initialize strikeCount and ballCount 
-score.scoreInit = function () {
-    strikeCount = 0;
-    ballCount = 0;
-}
-
 // Checking player is finished or not 
 score.isPlayerFinished = function (score) {
     return score === '안타' || score === '아웃' || this.strikeCount === 3 || this.ballCount ===4;
 }
+
+// Checking progression of game and Initializing game
+score.progress = function (score) {
+    if (this.isOver()){
+        this.scoreInit();
+        this.print(score);
+    } else if (this.isPlayerFinished(score)) {
+        this.scoreInit();
+        this.print(score);
+        this.init();
+    } else if (score === '스트라이크' || score ==='볼'){
+        this.print(score);
+        this.init();
+    }
+}
+
+// When player changed, initialize strikeCount and ballCount 
+score.scoreInit = function () {
+    this.strikeCount = 0;
+    this.ballCount = 0;
+}
+
 
 // Update count of score
 score.updateScore =function (score) {
@@ -59,28 +77,14 @@ score.updateScore =function (score) {
     } 
 }
 
-// Checking progressiong of game and initialize game
-score.progress = function (score) {
-    if (this.isPlayerFinished(score)) {
-        this.scoreInit();
-        this.print();
-        this.init();
-    } else if (score === '스트라이크' || score ==='볼'){
-        this.print();
-        this.init();
-    }
-    if (this.isOver()){
-        this.print();
-    }
-}
 
 // Pick random score in scoreList 
 score.pickRandomScore = function (){
     const randomNum = Math.floor(Math.random() * 4); // 0, 1, 2, 3
     const score = this.scoreList[randomNum] // 스크라이크
     this.updateScore(score); // strikeCount++;
-    this.checkAccumulate(); // 스트라이크 3개면 아웃 1개
-    this.print(score);
+    this.checkAccumulation(); // 스트라이크 3개면 아웃 1개
+    // this.print(score);
     this.progress(score);
 };
 
@@ -88,7 +92,8 @@ score.pickRandomScore = function (){
 score.init = function () {
     if(this.playCount === 0) {
         this.playCount++;
-        console.log(`첫 번째 타자가 타석에 입장했습니다.`);
+        console.log(`신나는 야구게임!`);
+        console.log(`첫 번째 타자가 타석에 입장했습니다.\n`);
     }
     this.pickRandomScore();
 }
