3b5558d28e0ce580fa7668360b4f64c20ff3c26e
Son <guswns1659@gmail.com>
2019-12-11 12:51:30 +0900

    [Level3] 4th   Create layout of scoreboard2

---
+++ SBO.js
@@ -0,0 +1,8 @@
+const currTeam = document.querySelector('.currTeam'),
+    printSBO = document.querySelector('.printSBO'),
+    printHSH = document.querySelector('.printHSH');
+
+function init() {
+
+}
+init();
