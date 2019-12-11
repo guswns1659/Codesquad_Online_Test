b183a171e1bfb4e5b7e5dc071e2688106f78eafd
Son <guswns1659@gmail.com>
2019-12-11 10:57:05 +0900

    [level3] 2nd   Create layout of scoreboard2

--- layout.js
+++ layout.js
@@ -3,7 +3,51 @@ const board1Container = document.querySelector('.board1Container'),
     board1Row2 = board1Container.querySelector('#row2'),
     board1Row3 = board1Container.querySelector('#row3');
 
-function init() {
+// cell의 css속성이 담긴 className
+const CELL_CN = 'board1Cell';
 
+// 전광판의 1회, 2회 ... 
+function makeCell1() {
+    for(let i = 0; i<8; i++){
+        const cell = document.createElement('div');
+        cell.classList.add(CELL_CN);
+        cell.innerHTML = `${i}회`;
+        cell.id = `cell${i}`;
+        board1Row1.appendChild(cell);
+    }
+    // copyText(board1Row1);
+    const cell7 = board1Row1.querySelector('#cell7');
+    cell7.innerHTML = `Total`;
+    const cell0 = board1Row1.querySelector('#cell0');
+    cell0.innerHTML = `전광판`;
+}
+// 전광판 속 team1의 점수
+function makeCell2() {
+    copyText(board1Row2);
+    const cell0 = board1Row2.querySelector('#cell0');
+    cell0.innerHTML = `${input.teamName[0]}`;
+}
+
+// 전광판 속 team2의 점수
+function makeCell3() {
+    copyText(board1Row3);
+    const cell0 = board1Row3.querySelector('#cell0');
+    cell0.innerHTML = `${input.teamName[1]}`;
+}
+
+function copyText(row) {
+    for(let i = 0; i<8; i++){
+        const cell = document.createElement('div');
+        cell.classList.add(CELL_CN);
+        cell.innerHTML = `0`;
+        cell.id = `cell${i}`;
+        row.appendChild(cell);
+    }
+}
+
+function init() {
+    makeCell1();
+    makeCell2();
+    makeCell3();
 }
 init();
